f22cadc21e168fee98f44babc3b713629c3b4c49
Madhumeeta bhatia <madhumeetabhatia@gmail.com>
2023-09-20 19:36:04 +0530

    added git-demo project to local repo

---
+++ src/main/java/com/madhumita/git/GitDemo/App.java
@@ -0,0 +1,18 @@
+package com.madhumita.git.GitDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{  public void addData(String data)
+	{
+	 System.out.print("Data"+ data);
+	
+	
+	}
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
